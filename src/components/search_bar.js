56a26ad9826c1d7ab1f1ec3598b0f958cd453249
Kyle Shin <kdshin2010@gmail.com>
2016-11-11 17:55:52 -0800

    refactored searchbar

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,8 +1,18 @@
-import React from 'react'; //React creates each element so we need to import React
+import React, {Component} from 'react'; //React creates each element so we need to import React
+//{Component} creates variable component from React Class
 
+/* Refactor from functional component (below)
 const SearchBar = () => {
   return <input /> //generate input
 };
+
+to
+*/
+class SearchBar extends Component { //enables our searchbar to access React.Component Class
+  render() {
+    return <input />;
+  }
+}
 
 //export files to allow index.js to access this file
 export default SearchBar;
